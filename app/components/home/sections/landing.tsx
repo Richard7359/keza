a5bf12329a191bbece6461a436d6340a5f3d5c67
RichardIshimwe <ishimwerichard26@gmail.com>
2024-04-09 02:53:42 +0200

    fix : home page welcoming section

--- app/components/home/sections/landing.tsx
+++ app/components/home/sections/landing.tsx
@@ -41,12 +41,33 @@ export default function Landing() {
     <section className="w-[100%]">
       <div className="flex justify-center mt-2">
         <div className="relative w-[95%] h-[70vh] rounded-2xl">
-        <video src='/keza_video_original.mp4' autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-2xl"/>
-        <div className="absolute inset-0 bg-black rounded-2xl"></div>
+          <video
+            src="/keza_video_original.mp4"
+            autoPlay
+            muted
+            loop
+            className="absolute top-0 left-0 w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 bg-black opacity-60 rounded-2xl"></div>
+          <div className="absolute inset-0 flex items-end m-4 text-white">
+            <div className="max-w-[40%]">
+              <h1 className="text-7xl font-bold">
+                Welcome to Keza Education Future Lab.
+              </h1>
+              <p className="my-7 text-xl font-normal">
+                <strong>Keza Education Future Lab [ KEFL ]</strong> is a
+                learning platform that powers young kids of 3 to 14 years of age
+                to become future innovators using technology such as robotics,
+                programming and STEAM learning.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <Scroling_patners />
-      <p className="text-center pt-2"><strong>KEFL</strong> is trusted and partnering with these organizations</p>
+      <p className="text-center pt-2">
+        <strong>KEFL</strong> is trusted and partnering with these organizations
+      </p>
     </section>
   );
 }
