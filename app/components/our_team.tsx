70c1144044b0f8844a7e25bbfbe64a036ba4d499
RichardIshimwe <ishimwerichard26@gmail.com>
2024-04-15 22:41:49 +0200

    feature view full profile feature

--- app/components/our_team.tsx
+++ app/components/our_team.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import Antoine from "../images/Antoine-MUTSINZI.png";
 import Celestin from "../images/celestin_image.png";
 import Erneste from "../images/erneste-niyigena.png";
-import Emile from "../images/MUKIZA-Emile.png";
 import Ariela from "../images/ariela.png";
+import Link from "next/link";
 
 const OurTeam = () => {
   return (
@@ -418,23 +418,25 @@ const OurTeam = () => {
         </div>
       </div>
       <div className="flex justify-center mb-4">
-        <button className="cssbuttons-io-button">
-          View full profile
-          <div className="icon py-3 px-4">
-            <svg
-              height="20"
-              width="20"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path
-                d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                fill="currentColor"
-              ></path>
-            </svg>
-          </div>
-        </button>
+        <Link href="/team" legacyBehavior passHref>
+          <button className="cssbuttons-io-button">
+            View full profile
+            <div className="icon py-3 px-4">
+              <svg
+                height="20"
+                width="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path
+                  d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+          </button>
+        </Link>
       </div>
     </div>
   );
