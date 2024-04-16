460698b3505265422af12a0bb10e60d024ab1f6d
RichardIshimwe <ishimwerichard26@gmail.com>
2024-04-16 20:05:30 +0200

    updated form inputs ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,19 +5,22 @@ import TextIpunt from "@/app/components/input";
 const page = () => {
   return (
     <div className="grid grid-cols-2 h-[100vh]">
-      <div className="bg-green flex justify-center items-center">
-        <div className="flex flex-wrap content-center justify-center rounded-l-md bg-white  w-[80%]">
+      <div className="bg-green flex">
+        <div className="bg-white w-full">
+        {/* <div className="flex content-center justify-center rounded-l-md bg-white"> */}
           <div className="">
-            <h1 className="text-xl font-semibold">Welcome back</h1>
-            <small className="text-gray-400">
+            {/* <h1 className="text-xl font-semibold">Welcome back</h1>
+            <small className="text-gray-400 bg-green">
               Welcome back! Please enter your details
-            </small>
+            </small> */}
+                            <label className="text-sm">Email Address</label>
+                <input type="text" className="w-full p-2 mt-2 rounded-md border border-black" />
 
-            <form className="mt-4">
-              <div className="mb-3">
-               <TextIpunt />
+            {/* <form className="">
+              <div className="w-full">
+                <label className="text-sm">Email Address</label>
+                <input type="text" className="w-full p-2 mt-2 rounded-md border border-black" />
               </div>
-
               <div className="mb-3 flex flex-wrap content-center">
                 <input
                   id="remember"
@@ -40,19 +43,18 @@ const page = () => {
                   Sign in
                 </button>
                 <button className="flex flex-wrap justify-center w-full border border-gray-300 hover:border-gray-500 px-2 py-1.5 rounded-md">
-                  {/* <img className="w-5 mr-2" src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"> */}
                 </button>
               </div>
-            </form>
+            </form> */}
 
-            <div className="text-center">
+            {/* <div className="text-center">
               <span className="text-xs text-gray-400 font-semibold">
                 Don&apos;t have account?
               </span>
               <a href="#" className="text-xs font-semibold text-purple-700">
                 Sign up
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
