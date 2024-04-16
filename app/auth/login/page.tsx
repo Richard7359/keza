6551cfb192fec44826ab0783fcd2956e96c6c33c
RichardIshimwe <ishimwerichard26@gmail.com>
2024-04-17 01:31:45 +0200

    login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,60 +1,84 @@
 import Image from "next/image";
 import bootcamp from "../../images/bootcamp-image-2.jpg";
-import TextIpunt from "@/app/components/input";
+import Google from "../../images/google.png";
+import Keza_logo from "../../images/burgundy-logo.webp";
 
 const page = () => {
   return (
     <div className="grid grid-cols-2 h-[100vh]">
       <div className="bg-green flex">
         <div className="bg-white w-full">
-        {/* <div className="flex content-center justify-center rounded-l-md bg-white"> */}
+          <div></div>
+          {/* <div className="flex content-center justify-center rounded-l-md bg-white"> */}
           <div className="">
-            {/* <h1 className="text-xl font-semibold">Welcome back</h1>
-            <small className="text-gray-400 bg-green">
-              Welcome back! Please enter your details
-            </small> */}
-                            <label className="text-sm">Email Address</label>
-                <input type="text" className="w-full p-2 mt-2 rounded-md border border-black" />
-
-            {/* <form className="">
-              <div className="w-full">
-                <label className="text-sm">Email Address</label>
-                <input type="text" className="w-full p-2 mt-2 rounded-md border border-black" />
-              </div>
-              <div className="mb-3 flex flex-wrap content-center">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  className="mr-1 checked:bg-purple-700"
-                />{" "}
-                <label
-                  htmlFor="remember"
-                  className="mr-auto text-xs font-semibold"
-                >
-                  Remember for 30 days
-                </label>
-                <a href="#" className="text-xs font-semibold text-purple-700">
-                  Forgot password?
+            <div className="flex justify-center items-center h-[100vh]">
+              <div className="w-[70%] ">
+                <a href="/">
+                <Image
+                  src={Keza_logo}
+                  alt="KEFL Logo image"
+                  className="w-15 md:w-12 object-cover cursor-pointer mb-6"
+                />
                 </a>
-              </div>
-
-              <div className="mb-3">
-                <button className="mb-1.5 block w-full text-center text-white bg-purple-700 hover:bg-purple-900 px-2 py-1.5 rounded-md">
-                  Sign in
+                <h1 className="text-xl font-semibold">Welcome back</h1>
+                <small className="text-gray-400 text-center">
+                  So glad to have you here! Please enter your credentials!{" "}
+                </small>
+                <br />
+                <button className="flex justify-center items-center mb-1.5 mt-8 w-full text-center text-black font-semibold text-sm border border-burgundy cursor-pointer  px-2 py-1.5 rounded-2xl">
+                  <Image
+                    src={Google}
+                    alt="google image image"
+                    className="w-[30px] h-[30px] rounded-lg bg-red-500"
+                  />
+                  continue with google
                 </button>
-                <button className="flex flex-wrap justify-center w-full border border-gray-300 hover:border-gray-500 px-2 py-1.5 rounded-md">
+                <label className="text-sm">Email Address</label>
+                <input
+                  type="text"
+                  className="w-full p-2 mb-2 rounded-lg border border-black"
+                />
+                <label className="text-sm">Password</label>
+                <input
+                  type="text"
+                  className="w-full p-2 mb-2 rounded-lg border border-black"
+                />
+                <div className="mb-3 flex flex-wrap content-center">
+                  <input
+                    id="remember"
+                    type="checkbox"
+                    className="mr-1 checked:text-burgundy"
+                  />{" "}
+                  <label
+                    htmlFor="remember"
+                    className="mr-auto text-xs font-semibold"
+                  >
+                    Remember for 30 days
+                  </label>
+                  <a href="#" className="text-xs font-semibold text-burgundy">
+                    Forgot password?
+                  </a>
+                </div>
+                <button className="mb-1.5 block w-full text-center text-white bg-burgundy cursor-pointer hover:bg-burgundy-light text-black px-2 py-1.5 rounded-2xl">
+                  Login
                 </button>
+                <div className="text-center">
+                  <span className="text-xs text-gray-400 font-semibold">
+                    Don&apos;t have account?
+                  </span>
+                  {/* <a
+                    href="/auth/signup"
+                    className="text-xs font-semibold text-burgundy"
+                  > */}
+                  <a
+                    href="#"
+                    className="text-xs font-semibold text-burgundy"
+                  >
+                    Sign up
+                  </a>
+                </div>
               </div>
-            </form> */}
-
-            {/* <div className="text-center">
-              <span className="text-xs text-gray-400 font-semibold">
-                Don&apos;t have account?
-              </span>
-              <a href="#" className="text-xs font-semibold text-purple-700">
-                Sign up
-              </a>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
