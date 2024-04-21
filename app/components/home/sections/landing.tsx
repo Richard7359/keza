6a361ab6f9a0257d3f8ea067ada5476d930dc8d7
RichardIshimwe <ishimwerichard26@gmail.com>
2024-04-22 01:43:20 +0200

    fix: landing page responsiveness fix

--- app/components/home/sections/landing.tsx
+++ app/components/home/sections/landing.tsx
@@ -13,13 +13,13 @@ export default function Landing() {
             className="absolute top-0 left-0 w-full h-full object-cover"
           />
           <div className="absolute flex justify-center items-end h-full w-full">
-            <div className="bg-[#cfddf0] md:bg-white px-4 py-2 rounded-[10px]  md:opacity-[0.85] mb-4 text-center">
+            <div className="bg-[#cfddf0] w-[90%] md:w-[30%] px-4 py-2 rounded-[10px]  md:opacity-[0.85] mb-4 text-center">
               <h1 className="font-bold text-black">
                 Keza Education Future Lab [KEFL]
               </h1>
               <p className="font-normal text-black">
               a learning
-          platform that powers young kids of 3 to 14 years of age to become <br />
+          platform that powers young kids of 3 to 14 years of age to become 
           future innovators using technology .
               </p>
             </div>
