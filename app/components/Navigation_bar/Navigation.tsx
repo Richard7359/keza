e8ad84aa9e1e2ba4a916494c4895d30b597b6b14
RichardIshimwe <ishimwerichard26@gmail.com>
2024-04-25 17:39:13 +0200

    card item

--- app/components/Navigation_bar/Navigation.tsx
+++ app/components/Navigation_bar/Navigation.tsx
@@ -22,7 +22,7 @@ export default function Navigation() {
   const [active, setActive] = useState(true);
 
   return (
-    <nav className="bg-burgundy static-position border-gray-200 dark:bg-gray-900 ">
+    <nav className="bg-burgundy static-position dark:bg-gray-900 ">
       <div className="flex flex-wrap items-center justify-between md:justify-around mx-auto p-4">
         <a
           href="/"
@@ -66,7 +66,7 @@ export default function Navigation() {
           // id="navbar-default"
         >
           {/* <div className="hidden w-full md:block md:w-auto" id="navbar-default"> */}
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:mx-2 border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:mx-2  rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
                 href="/"
@@ -137,7 +137,7 @@ export default function Navigation() {
               </a>
             </li>
 
-            <button className="bg-burgundy mt-2 md:mt-0 text-white md:bg-white md:text-burgundy px-8 py-3 rounded-md text-md font-bold">
+            <button className="bg-burgundy mt-2 md:mt-0 text-white md:bg-white md:text-burgundy px-8 py-2 rounded-md text-md font-bold">
               {/* Get in touch */}
               TALK TO KFL
             </button>
--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -14,52 +14,80 @@ import {
 } from "@/components/ui/breadcrumb";
 import { HiOutlineChevronDoubleRight } from "react-icons/hi";
 
+import Ichigo from "../images/ichigojam.jpg";
+import Image from "next/image";
+import { IoStar } from "react-icons/io5";
+import { FaRegStar } from "react-icons/fa";
+import { GoDotFill } from "react-icons/go";
 
 const Page = () => {
   const [selected, setSelected] = useState(0);
+  const [stared, setStared] = useState(false);
   return (
-    <div>
+    <div className="bg-[#f7f8fa] h-screen">
       <Navigation />
-      <div className="mx-3 my-6">
-        <Breadcrumb>
-          <BreadcrumbList className="">
-                          
-            <BreadcrumbItem onClick={() => setSelected(0)} className={`cursor-pointer text-all font-bold  px-4 py-1 border border-transparent hover:border-all hover:bg-all ${selected == 0 && "border-all bg-all bg-opacity-5"} hover:bg-opacity-5 rounded-full`}>
-              <BreadcrumbLink>
-                <Link href="">All levels</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <HiOutlineChevronDoubleRight className="text-all"/>
-            
-            <BreadcrumbItem onClick={() => setSelected(1)} className={`cursor-pointer text-level1 font-bold  px-4 py-1 border border-transparent hover:border-level1 hover:bg-level1 ${selected == 1 && "border-level1 bg-level1 bg-opacity-5"} hover:bg-opacity-5 rounded-full`}>
-              <BreadcrumbLink>
-                <Link href="">Beginner level</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <HiOutlineChevronDoubleRight  className="text-level1" />
-            
-            <BreadcrumbItem onClick={() => setSelected(2)} className={`cursor-pointer text-level2 font-bold  px-4 py-1 border border-transparent hover:border-level2 hover:bg-level2 ${selected == 2 && "border-level2 bg-level2 bg-opacity-5"} hover:bg-opacity-5 rounded-full`}>
-              <BreadcrumbLink>
-                <Link href="">Middle level</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <HiOutlineChevronDoubleRight className="text-level2"/>
-            
-            <BreadcrumbItem onClick={() => setSelected(3)} className={`cursor-pointer text-level3 font-bold  px-4 py-1 border border-transparent hover:border-level3 hover:bg-level3 ${selected == 3 && "border-level3 bg-level3 bg-opacity-5"} hover:bg-opacity-5 rounded-full`}>
-              <BreadcrumbLink>
-                Pre-advanced level
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <HiOutlineChevronDoubleRight className="text-level3"/>
+      <div className="flex justify-center">
+        <div className="my-6 w-[97%] bg-white py-6 px-6 rounded-keza border-custom border-custom">
+          <Breadcrumb>
+            <BreadcrumbList className="">
+              <BreadcrumbItem
+                onClick={() => setSelected(0)}
+                className={`cursor-pointer text-all font-bold  px-4 py-1 border border-transparent hover:border-all hover:bg-all ${
+                  selected == 0 && "border-all bg-all bg-opacity-5"
+                } hover:bg-opacity-5 rounded-full`}
+              >
+                <BreadcrumbLink>
+                  <Link href="">All levels</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <HiOutlineChevronDoubleRight className="text-all" />
 
-            <BreadcrumbItem onClick={() => setSelected(4)} className={`cursor-pointer text-level4 font-bold  px-4 py-1 border border-transparent hover:border-level4 hover:bg-level4 ${selected == 4 && "border-level4 bg-level4 bg-opacity-5"} hover:bg-opacity-5 rounded-full`}>
-              <BreadcrumbLink>
-                <Link href="">Advanced level</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+              <BreadcrumbItem
+                onClick={() => setSelected(1)}
+                className={`cursor-pointer text-level1 font-bold  px-4 py-1 border border-transparent hover:border-level1 hover:bg-level1 ${
+                  selected == 1 && "border-level1 bg-level1 bg-opacity-5"
+                } hover:bg-opacity-5 rounded-full`}
+              >
+                <BreadcrumbLink>
+                  <Link href="">Beginner level</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <HiOutlineChevronDoubleRight className="text-level1" />
 
+              <BreadcrumbItem
+                onClick={() => setSelected(2)}
+                className={`cursor-pointer text-level2 font-bold  px-4 py-1 border border-transparent hover:border-level2 hover:bg-level2 ${
+                  selected == 2 && "border-level2 bg-level2 bg-opacity-5"
+                } hover:bg-opacity-5 rounded-full`}
+              >
+                <BreadcrumbLink>
+                  <Link href="">Middle level</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <HiOutlineChevronDoubleRight className="text-level2" />
 
-            {/* <BreadcrumbItem>
+              <BreadcrumbItem
+                onClick={() => setSelected(3)}
+                className={`cursor-pointer text-level3 font-bold  px-4 py-1 border border-transparent hover:border-level3 hover:bg-level3 ${
+                  selected == 3 && "border-level3 bg-level3 bg-opacity-5"
+                } hover:bg-opacity-5 rounded-full`}
+              >
+                <BreadcrumbLink>Pre-advanced level</BreadcrumbLink>
+              </BreadcrumbItem>
+              <HiOutlineChevronDoubleRight className="text-level3" />
+
+              <BreadcrumbItem
+                onClick={() => setSelected(4)}
+                className={`cursor-pointer text-level4 font-bold  px-4 py-1 border border-transparent hover:border-level4 hover:bg-level4 ${
+                  selected == 4 && "border-level4 bg-level4 bg-opacity-5"
+                } hover:bg-opacity-5 rounded-full`}
+              >
+                <BreadcrumbLink>
+                  <Link href="">Advanced level</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+
+              {/* <BreadcrumbItem>
               <BreadcrumbLink>
                 <Link href="/">Pre-advanced Level</Link>
               </BreadcrumbLink>
@@ -70,10 +98,29 @@ const Page = () => {
                 <Link href="/">Advanced Level</Link>
               </BreadcrumbLink>
             </BreadcrumbItem> */}
-            
-          </BreadcrumbList>
-         
-        </Breadcrumb>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <p className="py-2 font-bold">Recent added courses.</p>
+          <div className="h-[225px] w-[300px] border-custom rounded-2xl">
+            <div className="h-[60%] rounded-t-2xl">
+              <Image
+                src={Ichigo}
+                alt="KEFL Logo image"
+                className="w-full h-full object-cover cursor-pointer rounded-t-[16px] border-custom"
+              />
+            </div>
+            <div className="h-[25%] flex justify-between mx-3 py-1">
+              <p className="font-serif text-sm">Ichigo Jam</p>
+              {stared ? <IoStar /> : <FaRegStar />}{" "}
+            </div>
+            <div className="bg-blue-light h-[15%] rounded-b-2xl flex items-center justify-end">
+              <div className="inline-flex border border-black items-center mx-2 px-2 rounded-full ">
+                <GoDotFill />
+                <p className="text-[12px] font-bold">EASY</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
