4fabfa3932026e7c3f2648a5b60d181e9cbfc129
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-01 21:06:59 +0200

    single course page

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Navigation from "@/app/components/Navigation_bar/Navigation";
 import Footer from "@/app/components/Footer";
-import image from "@/app/images/ichigojam.jpg";
+import image from "../../images/ichigojam.jpg";
 import Image from "next/image";
 
 const Page = () => {
@@ -13,14 +13,27 @@ const Page = () => {
     <>
       <Navigation />
       <div className="">
-        <p className="text-center text-2xl font-bold mt-10">Ichigo Jam</p>
-        <p className="text-center mt-2">BY Emma on Feb 6, 2020</p>
-        <div>
-        <Image
-          src={image}
-          alt="KEFL Logo image"
-          className="w-full h-full object-cover cursor-pointer rounded-t-[16px] border-custom"
-        />
+        <div className="mb-4">
+          <p className="text-center text-2xl font-bold mt-10">Ichigo Jam</p>
+          <p className="text-center mt-1">BY Emma on Feb 6, 2020</p>
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src={image}
+            alt="KEFL Logo image"
+            className="w-[500px] h-[300px] object-cover cursor-pointer rounded-[5px] border-custom"
+          />
+        </div>
+        <div className="flex justify-center mt-4">
+          <div className="w-[40%]">
+            <p className="text-2xl font-bold mt-10">Description</p>
+            <p className="mt-1">
+              IchigoJam is a simple computer that you can start easily. It has
+              BASIC language and you can enjoy programming with it. IchigoJam is
+              very small and easy to use. It is good for beginners who want to
+              learn programming.
+            </p>
+          </div>
         </div>
       </div>
       {/* <Footer /> */}
