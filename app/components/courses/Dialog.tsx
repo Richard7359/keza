2a678755f7258c6dde9ac600ef5514e2b2772e88
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-07 21:42:09 +0200

    view one course section

--- app/components/courses/Dialog.tsx
+++ app/components/courses/Dialog.tsx
@@ -6,19 +6,39 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from "next/image";
+import image from "../../images/ai2.png";
 
 const ViewMaterials = () => {
   return (
     <Dialog>
-      <DialogTrigger className="border border-level4 font-semibold bg-level4 bg-opacity-5 hover:border-level4 hover:bg-level4 hover:text-white rounded-full text-sm px-4 py-1">View materials</DialogTrigger>
+      <DialogTrigger className="border border-level4 font-semibold bg-level4 bg-opacity-5 hover:border-level4 hover:bg-level4 hover:text-white rounded-full text-sm px-4 py-1">
+        View materials
+      </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
+        <div className="grid grid-cols-3">
+          <div className="bg-green">
+            <Image
+              src={image}
+              alt="KEFL Logo image"
+              className="w-[130px] h-[130px] object-cover cursor-pointer rounded-[5px] border-custom hover:w-[170px] hover:h-[170px] hover:scale-105 transition duration-500 ease-in-out"
+            />
+          </div>
+          <div>
+            <Image
+              src={image}
+              alt="KEFL Logo image"
+              className="w-[130px] h-[130px] object-cover cursor-pointer rounded-[5px] border-custom hover:w-[170px] hover:h-[170px] hover:scale-105 transition duration-500 ease-in-out"
+            />
+          </div>
+        </div>
+        {/* <DialogHeader>
           <DialogTitle>Are you absolutely sure?</DialogTitle>
           <DialogDescription>
             This action cannot be undone. This will permanently delete your
             account and remove your data from our servers.
           </DialogDescription>
-        </DialogHeader>
+        </DialogHeader> */}
       </DialogContent>
     </Dialog>
   );
--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -6,9 +6,28 @@ import Footer from "@/app/components/Footer";
 import image from "../../images/ichigojam.jpg";
 import Image from "next/image";
 import ViewMaterials from "@/app/components/courses/Dialog";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { TbArrowBadgeDown } from "react-icons/tb";
+import { GiPoliceBadge } from "react-icons/gi";
+import { SlBadge } from "react-icons/sl";
+// import { SlBadge } from "react-icons/sl";
+import { BiBadge } from "react-icons/bi";
+import { BiSolidBadge } from "react-icons/bi";
 
 const Page = () => {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState("null");
+  const [oneSelected, oneSetSelected] = useState(false);
+  const [twoSelected, twoSetSelected] = useState(false);
+  const [threeSelected, threeSetSelected] = useState(false);
+  const [fourSelected, fourSetSelected] = useState(false);
+  // const [selected, setSelected] = useState("null");
+  // const [selected, setSelected] = useState("null");
+  // const [selected, setSelected] = useState("null");
 
   return (
     <>
@@ -27,14 +46,151 @@ const Page = () => {
         </div>
         <div className="flex justify-center mt-4">
           <div className="w-[40%]">
-            {/* <p className="text-2xl font-bold mt-10">Description</p>
-            <p className="mt-1">
-              IchigoJam is a simple computer that you can start easily. It has
-              BASIC language and you can enjoy programming with it. IchigoJam is
-              very small and easy to use. It is good for beginners who want to
-              learn programming.
-            </p> */}
             <ViewMaterials />
+            <div className="mt-4">
+              <div className="flex justify-center">
+                <Accordion
+                  type="single"
+                  collapsible
+                  className="w-[100%] border-none"
+                >
+                  <AccordionItem
+                    value="item-1"
+                    className="border border-none"
+                    onClick={() => {
+                      oneSetSelected(!oneSelected);
+                      twoSetSelected(false);
+                      threeSetSelected(false);
+                      fourSetSelected(false);
+                    }}
+                  >
+                    <AccordionTrigger
+                      className={`pd_id _c uc pc_id hover:uf hover:se ${
+                        oneSelected == true
+                          ? "pc_id_courses_bottom uf_course se_course"
+                          : ""
+                      }`}
+                    >
+                      <div className="flex items-center">
+                        <TbArrowBadgeDown className="text-3xl" /> Which level of
+                        the kids do you accept?
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent
+                      className={`pd_id _c uc ${
+                        oneSelected == true ? "pc_id_courses" : ""
+                      } sm:pd_id gd`}
+                    >
+                      <strong>KEFL</strong> is primarily focused on the kids
+                      that are in the age of 3 to 14 years old. Most of the time
+                      those kids are pursuing their primary level and
+                      O&apos;Level section of their normal education.
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem
+                    value="item-2"
+                    className="border border-none mt-4"
+                    onClick={() => {
+                      oneSetSelected(false);
+                      twoSetSelected(!twoSelected);
+                      threeSetSelected(false);
+                      fourSetSelected(false);
+                    }}
+                  >
+                    <AccordionTrigger
+                      className={`pd_id _c uc pc_id hover:uf hover:se ${
+                        twoSelected == true
+                          ? "pc_id_courses_bottom uf_course se_course"
+                          : ""
+                      }`}
+                    >
+                      <div className="flex items-center">
+                      <GiPoliceBadge className="text-3xl"/> 
+                      Which level of
+                        the kids do you accept?
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent
+                      className={`pd_id _c uc ${
+                        twoSelected == true ? "pc_id_courses" : ""
+                      } sm:pd_id gd`}
+                    >
+                      <strong>KEFL</strong> is primarily focused on the kids
+                      that are in the age of 3 to 14 years old. Most of the time
+                      those kids are pursuing their primary level and
+                      O&apos;Level section of their normal education.
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem
+                    value="item-3"
+                    className="border border-none mt-4"
+                    onClick={() => {
+                      oneSetSelected(false);
+                      twoSetSelected(false);
+                      threeSetSelected(!threeSelected);
+                      fourSetSelected(false);
+                    }}
+                  >
+                    <AccordionTrigger
+                      className={`pd_id _c uc pc_id hover:uf hover:se ${
+                        threeSelected == true
+                          ? "pc_id_courses_bottom uf_course se_course"
+                          : ""
+                      }`}
+                    >
+                      <div className="flex items-center">
+                      <SlBadge className="text-3xl"/> 
+                      Which level of
+                        the kids do you accept?
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent
+                      className={`pd_id _c uc ${
+                        threeSelected == true ? "pc_id_courses" : ""
+                      } sm:pd_id gd`}
+                    >
+                      <strong>KEFL</strong> is primarily focused on the kids
+                      that are in the age of 3 to 14 years old. Most of the time
+                      those kids are pursuing their primary level and
+                      O&apos;Level section of their normal education.
+                    </AccordionContent>
+                  </AccordionItem>
+                  {/* <AccordionItem value="item-1" className="border-none  mb-2">
+                    <AccordionTrigger className="pd_id _c uc pc_id hover:uf hover:se">
+                    <div className="flex items-center"><BiBadge className="text-3xl text-green"/> Which level of the kids do you accept?</div>
+                    </AccordionTrigger>
+                    <AccordionContent className="pd_id _c uc pc_id sm:pd_id gd my-3">
+                      <strong>KEFL</strong> is primarily focused on the kids
+                      that are in the age of 3 to 14 years old. Most of the time
+                      those kids are pursuing their primary level and
+                      O&apos;Level section of their normal education.
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem value="item-1" className="border-none  mb-2">
+                    <AccordionTrigger className="pd_id _c uc pc_id hover:uf hover:se">
+                    <div className="flex items-center"><BiSolidBadge className="text-3xl"/> Which level of the kids do you accept?</div>
+                    </AccordionTrigger>
+                    <AccordionContent className="pd_id _c uc pc_id sm:pd_id gd my-3">
+                      <strong>KEFL</strong> is primarily focused on the kids
+                      that are in the age of 3 to 14 years old. Most of the time
+                      those kids are pursuing their primary level and
+                      O&apos;Level section of their normal education.
+                    </AccordionContent>
+                  </AccordionItem>
+                  <AccordionItem value="item-1" className="border-none  mb-2">
+                    <AccordionTrigger className="pd_id _c uc pc_id hover:uf hover:se">
+                    <div className="flex items-center"><SlBadge className="text-3xl"/> Which level of the kids do you accept?</div>
+                    </AccordionTrigger>
+                    <AccordionContent className="pd_id _c uc pc_id sm:pd_id gd my-3">
+                      <strong>KEFL</strong> is primarily focused on the kids
+                      that are in the age of 3 to 14 years old. Most of the time
+                      those kids are pursuing their primary level and
+                      O&apos;Level section of their normal education.
+                    </AccordionContent>
+                  </AccordionItem> */}
+                </Accordion>
+              </div>
+            </div>
           </div>
         </div>
       </div>
