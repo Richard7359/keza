64850128153bc385982f1971eea1108be67f257e
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-08 18:17:54 +0200

    dashboard : sidebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/dashboard/Header';
+import SideBar from "../components/dashboard/SideBar";
 
 const page = () => {
   return (
     <div>
       <Header />
+      <SideBar />
     </div>
   )
 }
