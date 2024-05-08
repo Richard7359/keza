5ab3c3e86a735b047efeb6488f0c57cc63b47fb0
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-08 17:47:52 +0200

    dashboard header

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/dashboard/Header';
+
+const page = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default page
