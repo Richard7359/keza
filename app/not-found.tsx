9f8079bd819f7a02db7d1639c7c348775c42bf59
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-08 12:05:46 +0200

    404 page fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ const page = () => {
       <section className="section error-404 min-vh-100 flex flex-col items-center justify-center">
         <h1>404</h1>
         <h2>The page you are looking for doesn&lsquo;t exist.</h2>
-       <button className="btn rounded-md"><a  href="/">Back to home</a></button>
+       <button className="btn"><a  href="/">Back to home</a></button>
         <Image
                 src={bootcamp}
                 alt="Page Not Found"
