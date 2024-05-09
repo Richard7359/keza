56e7fd75a73dc3a388df8de22648745f6383fb3b
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-09 18:35:35 +0200

    breadcrumb done

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Header from "../components/dashboard/Header";
 import SideBar from "../components/dashboard/SideBar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-
 import "../css/dashboard.css";
 import "../css/bootstrap.min.css";
 import "../css/bootstrap-icons.css";
 import "../css/style.css";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb";
+  
 
 const DashboardWrapper = ({
   children,
@@ -21,27 +21,12 @@ const DashboardWrapper = ({
   children?: React.ReactNode;
 }>) => {
   return (
-    <div>
+    <div className="">
       <Header />
       <SideBar />
-      <div className="sm:ml-[300px] sm:mt-[10px] fixed">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+      <div className="sm:ml-[150px] sm:mt-[60px] ">     
+        {children}
       </div>
-      <div className="sm:ml-[300px] sm:mt-[60px]">{children}</div>
     </div>
   );
 };
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,37 @@
 import DashboardWrapper from "./layout";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 const page = () => {
   return (
     <DashboardWrapper>
       <div>
-        
+        <div className="">
+          <Breadcrumb className="h-[45px] flex">
+            <BreadcrumbList className="w-full mb-0 breadcumb_shadow bg-white">
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </div>
+      <div>
+        hello
       </div>
     </DashboardWrapper>
   );
