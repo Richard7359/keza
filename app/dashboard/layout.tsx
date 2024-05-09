17d7d75ddb7cf08631f3c8ede5331db518270d4f
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-09 15:49:33 +0200

    feature : breadcrumb

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import Header from '../components/dashboard/Header';
+import React from "react";
+import Header from "../components/dashboard/Header";
 import SideBar from "../components/dashboard/SideBar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
 import "../css/dashboard.css";
 import "../css/bootstrap.min.css";
 import "../css/bootstrap-icons.css";
 import "../css/style.css";
 
 const DashboardWrapper = ({
-    children,
-  }: Readonly<{
-    children?: React.ReactNode;
-  }>) => {
+  children,
+}: Readonly<{
+  children?: React.ReactNode;
+}>) => {
   return (
     <div>
-        <Header />
-        <SideBar />
-      <div className='sm:ml-[300px] sm:mt-[60px]'>{children}</div>
+      <Header />
+      <SideBar />
+      <div className="sm:ml-[300px] sm:mt-[10px] fixed">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div className="sm:ml-[300px] sm:mt-[60px]">{children}</div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardWrapper
+export default DashboardWrapper;
