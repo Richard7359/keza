5b2b2fcff144907a803158ac4df6b38121712cc0
RichardIshimwe <ishimwerichard26@gmail.com>
2024-05-23 19:49:21 +0200

    updates on team page

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -43,9 +43,7 @@ const SampleGallery = () => {
               loading="lazy"
               className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
             />
-
             <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
-
             <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
               Workshop
             </span>
