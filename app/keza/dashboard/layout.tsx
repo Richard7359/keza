b185cef8b06048b53b06de90784fc37f60edbb29
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-10 01:49:20 +0200

    dashboard responsiveness

--- app/keza/dashboard/layout.tsx
+++ app/keza/dashboard/layout.tsx
@@ -194,7 +194,7 @@ const DashboardWrapper = ({
                   <Package className="h-5 w-5" />
                   Courses
                   <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                    6
+                    3
                   </Badge>
                 </Link>
                 <Link
@@ -236,7 +236,7 @@ const DashboardWrapper = ({
                 <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                 <Input
                   type="search"
-                  placeholder="Type in to search something"
+                  placeholder="Type here to search something"
                   className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
                 />
               </div>
