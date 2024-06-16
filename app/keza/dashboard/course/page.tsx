d2902029f0f557b6036e86015574e466f606b263
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-16 02:54:34 +0200

    feature : get only courses which are not archived

--- app/keza/dashboard/course/page.tsx
+++ app/keza/dashboard/course/page.tsx
@@ -92,14 +92,6 @@ function Page() {
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           <Tabs defaultValue="all">
             <div className="flex items-center">
-              {/* <TabsList>
-                <TabsTrigger value="all">All</TabsTrigger>
-                <TabsTrigger value="active">Active</TabsTrigger>
-                <TabsTrigger value="draft">Draft</TabsTrigger>
-                <TabsTrigger value="archived" className="hidden sm:flex">
-                  Archived
-                </TabsTrigger>
-              </TabsList> */}
               <div className="ml-auto flex items-center gap-2">
                 <DropdownMenu>
                   <DropdownMenuTrigger asChild>
