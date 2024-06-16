ff7f28df12b12664070dd3788780238a1d6c2d95
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-16 02:54:02 +0200

    feature : get only courses which are not archived

--- app/server/routes/course/getAllUnArchivedCourse.ts
+++ app/server/routes/course/getAllUnArchivedCourse.ts
@@ -4,7 +4,7 @@ import client from "@/app/utils/client";
 export const getAllUnArchivedCourse = router({
     getAllUnArchivedCourse: publicProcedure.query(async () => {
       try {
-       const allCourses =  await client.getAllCourses();
+       const allCourses =  await client.getAllUnArchivedCourse();
        return allCourses;
       } catch (error : any) {
         console.log(error);
