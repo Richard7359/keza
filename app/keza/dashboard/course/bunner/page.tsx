747ea17e6a674a21536b46e6194b42f7083c8e80
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-18 01:37:14 +0200

    feature add and render bunner

--- app/keza/dashboard/course/bunner/page.tsx
+++ app/keza/dashboard/course/bunner/page.tsx
@@ -14,6 +14,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { TbTrashX } from "react-icons/tb";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { GrCloudUpload } from "react-icons/gr";
@@ -55,6 +56,7 @@ function Page() {
       const data = await response.json();
       console.log(data);
     } catch (error) {
+      console.error(error);
       alert("error");
     }
   }
@@ -78,36 +80,62 @@ function Page() {
             <Input id="text" type="text" placeholder="" required />
           </div>
           <div className="grid gap-2">
-            <label className="opacity-1  text-xs font-bold hover:cursor-pointer">
-              Attach file
-              <input
-                type="file"
-                accept="application/pdf"
-                hidden={true}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  setPdf(e.target.files![0]);
-                }}
-              />
-              <div className="mt-2 flex h-52 flex-col items-center justify-center rounded-xl border border-dashed py-8 input_bg">
-                <span className="opacity-1 hover:shadow:sm flex h-20 w-20 items-center justify-center rounded-full bg-white">
-                  <GrCloudUpload
-                    size={34}
-                    className="text-primary-500 text-deepSkyBlue"
-                    strokeLinejoin="miter"
-                  />
-                </span>
-                <p className="mt-2 text-xs">
-                  Drop your files here or
-                  <span className="cursor-pointer text-deepSkyBlue underline">
-                    {" "}
-                    browse
+            {!pdf?.name ? (
+              <label className="opacity-1  text-xs font-bold hover:cursor-pointer">
+                Attach file
+                <input
+                  type="file"
+                  accept="application/pdf"
+                  hidden={true}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    console.log("selected pdf file");
+                    console.log(e.target.files![0]);
+                    setPdf(e.target.files![0]);
+                  }}
+                />
+                <div className="mt-2 flex h-52 flex-col items-center justify-center rounded-xl border border-dashed py-8 input_bg">
+                  <span className="opacity-1 hover:shadow:sm flex h-20 w-20 items-center justify-center rounded-full bg-white">
+                    <GrCloudUpload
+                      size={34}
+                      className="text-primary-500 text-deepSkyBlue"
+                      strokeLinejoin="miter"
+                    />
                   </span>
-                </p>
-                <p className="mt-2 text-xsm text_gray-400">
-                  Max file size 10MB, PDF, JPG,PNG or JPEG file supported.
-                </p>
+                  <p className="mt-2 text-xs">
+                    Drop your files here or
+                    <span className="cursor-pointer text-deepSkyBlue underline">
+                      {" "}
+                      browse
+                    </span>
+                  </p>
+                  <p className="mt-2 text-xsm text_gray-400">
+                    Max file size 10MB, PDF, JPG,PNG or JPEG file supported.
+                  </p>
+                </div>
+              </label>
+            ) : (
+              <div className="mt-1 flex flex-col  gap-2 ">
+                <label className="opacity-1 text-xs font-bold capitalize">
+                  Selected file
+                </label>
+                <div className="flex items-center justify-center gap-7 h-52">
+                  <span className="flex cursor-pointer text-sky-600 font-bold text-sm">
+                    {pdf?.name}
+                  </span>
+                  <button
+                    onClick={() => setPdf(null)}
+                    className="flex items-center justify-center gap-2 rounded-lg px-[5px] py-1 text-xs font-semibold"
+                    type="button"
+                  >
+                    <TbTrashX
+                      size={20}
+                      className="text-primary text-red"
+                      strokeLinejoin="miter"
+                    />
+                  </button>
+                </div>
               </div>
-            </label>
+            )}
           </div>
         </CardContent>
         <CardFooter>
