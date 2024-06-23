508abdc42d56e2f1b5d16ebfb09be9087db5a7f8
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-23 23:55:43 +0200

    deletes comments

--- app/keza/dashboard/course/bunner/page.tsx
+++ app/keza/dashboard/course/bunner/page.tsx
@@ -53,7 +53,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { FiDatabase } from "react-icons/fi";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import {
   Table,
   TableBody,
@@ -62,7 +61,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
 
 
 const FormSchema = z.object({
@@ -89,7 +88,7 @@ function Page() {
     const date = new Date(isoDateString);
   
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero indexed, so we add 1 to get the correct month number
+    const month = String(date.getMonth() + 1).padStart(2, "0"); 
     const day = String(date.getDate()).padStart(2, "0");
     const hours = Number(String(date.getHours()).padStart(2, "0"));
     const minutes = String(date.getMinutes()).padStart(2, "0");
