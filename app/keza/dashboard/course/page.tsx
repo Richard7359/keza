78ad238615a5e39d46b76db16dec0ecd64fa14d3
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-23 23:55:59 +0200

    deletes comments

--- app/keza/dashboard/course/page.tsx
+++ app/keza/dashboard/course/page.tsx
@@ -4,18 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 import {
   File,
-  Home,
-  LineChart,
   ListFilter,
   MoreHorizontal,
-  Package,
-  Package2,
-  PanelLeft,
   PlusCircle,
-  Search,
-  Settings,
-  ShoppingCart,
-  Users2,
 } from "lucide-react";
 
 import ichigo from "../../../images/TrafficLight/inner.jpeg";
