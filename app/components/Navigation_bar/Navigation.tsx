cf7d9646825ba8d1d011ce6b4ba6923698015f23
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-25 00:32:26 +0200

    feature single product page

--- app/components/Navigation_bar/Navigation.tsx
+++ app/components/Navigation_bar/Navigation.tsx
@@ -87,7 +87,7 @@ export default function Navigation() {
             </svg>
           </button>
           <div className={`w-full ${active && "hidden"} md:block md:w-auto`}>
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:mx-2  rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:mx-2  rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
                   href="/"
---
+++ app/keza/product/product_page.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import image from "../../images/bootcamp-image-1.jpg";
+import "../../css/product_css.css";
+
+const ProductPage = () => {
+  return (
+    <div className="min-h-[40vh]">
+      <div className="w-[350px]">
+        <div className="group relative border bg-white rounded-xl overflow-hidden">
+          {" "}
+          <div className="aspect-h-3 aspect-w-4 overflow-hidden bg-gray-100">
+            <Image
+              alt="GOOGLE PIXEL 6 PRO"
+              src={image}
+              // src="https://images.unsplash.com/photo-1706412703794-d944cd3625b3?q=80&amp;w=2940&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="object-cover object-center"
+            />
+            <div
+              className="flex items-end p-4 opacity-0 bg-white/10 backdrop-blur group-hover:opacity-100 duration-200"
+              aria-hidden="true"
+            >
+              {" "}
+              <div className="items-center text-sm group active:bg-gray-100 active:text-gray-900/60 active:transition-none border font-medium gap-2 inline-flex justify-center outline-offset-2 px-6 py-2 transition w-full bg-white hover:bg-gray-501 text-black1 rounded-lg">
+                Learn more
+              </div>{" "}
+            </div>{" "}
+          </div>{" "}
+          <div className="p-4">
+            {" "}
+            <div className="flex items-center justify-between space-x-8 text-base font-medium text-black1">
+              {" "}
+              <h3>
+                {" "}
+                <a
+                  href="/store/3"
+                  title="GOOGLE PIXEL 6 PRO"
+                  aria-label="GOOGLE PIXEL 6 PRO"
+                  className="font-display text-xl"
+                >
+                  {" "}
+                  <span
+                    aria-hidden="true"
+                    className="absolute inset-0"
+                  ></span>{" "}
+                  GOOGLE PIXEL 6 PRO{" "}
+                </a>{" "}
+              </h3>{" "}
+              <p>$19.99</p>{" "}
+            </div>{" "}
+            <p className="mt-1 text-sm text-gray-600">Phone cases</p>{" "}
+          </div>{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
