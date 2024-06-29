bf9d038395ca9c887a686c95d87dfbbacaf11603
RichardIshimwe <ishimwerichard26@gmail.com>
2024-06-29 15:31:09 +0200

    page : do it your self page initial commit

--- app/keza/diy/page.tsx
+++ app/keza/diy/page.tsx
@@ -12,9 +12,9 @@ import Ichigo from "../../images/ichigojam.jpg";
 
 const page = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen bg-green">
       <Navigation />
-      <div>this is diy page</div>
+      <div className="flex-grow">this is diy page</div>
       <Footer />
     </div>
   );
